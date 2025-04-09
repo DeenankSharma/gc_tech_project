98003e0da27c71b792aeb869c1cf14f0ec1c7659
DeenankSharma <sharmadeenank@gmail.com>
2025-04-09 17:03:12 +0530

    feat : adds preferences page

---
+++ frontend/src/constants.ts
@@ -0,0 +1,50 @@
+export const sportCategories = [
+  {
+    id: 'football',
+    name: 'Football',
+    description: 'Follow matches, transfers, and news from top leagues worldwide',
+    icon: '⚽'
+  },
+  {
+    id: 'basketball',
+    name: 'Basketball',
+    description: 'NBA, EuroLeague, and international basketball competitions',
+    icon: '🏀'
+  },
+  {
+    id: 'cricket',
+    name: 'Cricket',
+    description: 'Test matches, ODIs, T20s, and major tournaments',
+    icon: '🏏'
+  },
+  {
+    id: 'tennis',
+    name: 'Tennis',
+    description: 'Grand Slams, ATP, WTA tours and player updates',
+    icon: '🎾'
+  },
+  {
+    id: 'f1',
+    name: 'Formula 1',
+    description: 'Race results, driver standings, and team news',
+    icon: '🏎️'
+  },
+  {
+    id: 'baseball',
+    name: 'Baseball',
+    description: 'MLB, international games, and player transfers',
+    icon: '⚾'
+  },
+  {
+    id: 'golf',
+    name: 'Golf',
+    description: 'PGA Tour, European Tour, and major championships',
+    icon: '⛳'
+  },
+  {
+    id: 'hockey',
+    name: 'Hockey',
+    description: 'NHL, international tournaments and highlights',
+    icon: '🏒'
+  }
+];
--- frontend/src/router/Routes_.tsx
+++ frontend/src/router/Routes_.tsx
@@ -1,13 +1,15 @@
-import { Routes,Route, Router, BrowserRouter } from "react-router-dom";
+import { Routes,Route, BrowserRouter } from "react-router-dom";
 import { ChatPage } from "../pages/ChatPage.tsx";
 import { LoginPage } from "../pages/LoginPage.tsx";
 import { RegisterPage } from "../pages/RegisterPage.tsx";
+import Preferences from "../pages/Preferences.tsx";
 
 export const Routes_ = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<ChatPage />} />
+      <Route path="/p" element={<ChatPage />} />
+      <Route path="/" element={<Preferences />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
