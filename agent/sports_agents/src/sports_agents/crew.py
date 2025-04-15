8354ee8aafc64420dd8def0979aebe6b867135cb
DeenankSharma <sharmadeenank@gmail.com>
2025-04-15 19:31:42 +0530

    feat : adds player info agent

--- agent/sports_agents/src/sports_agents/crew.py
+++ agent/sports_agents/src/sports_agents/crew.py
@@ -1,5 +1,5 @@
-from crewai import Agent, Crew, Task, Process
-from crewai.project import CrewBase, agent,task, crew
+from crewai import LLM,Agent, Crew, Task, Process
+from crewai.project import CrewBase, agent,task, crew, after_kickoff
 from crewai_tools import WebsiteSearchTool,SerplyWebSearchTool,BraveSearchTool
 
 @CrewBase
@@ -17,14 +17,41 @@ class SportsChatbotCrew:
                 tools=[WebsiteSearchTool,SerplyWebSearchTool,BraveSearchTool],
                 llm='gemini/gemini-1.5-pro'
             )
+            
+    @agent 
+    def input_processor(self) -> Agent:
+        return Agent(
+            config=self.agents_config['input_processor'],
+            llm=LLM('gemini/gemini-1.5-pro',temperature=0),
+            verbose=True,
+        )
+        
+    @task
+    def structure_player_query(self) -> Task:
+        return Task(
+            config=self.agents_config['structure_player_query']
+        )
     
     @task
     def fetch_player_info_task(self) -> Task:
         return Task(
-            config=self.agents_config['fetch_player_info']
+            config=self.agents_config['fetch_player_info'],
+            context=["structure_player_query"]
         )
         
-        
+    @after_kickoff
+    def after_kickoff_function(self):
+        print(f"Hope u are satisfied by the results")
+    
+    @crew
+    def crew(self)->Crew:
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+            planning=True
+        )
         
     # @task
     # def live_match_updates_task():
--- agent/sports_agents/src/sports_agents/main.py
+++ agent/sports_agents/src/sports_agents/main.py
@@ -5,7 +5,7 @@ import warnings
 
 from datetime import datetime
 
-from crew import  SportsChatbotCrew
+from sports_agents.crew import  SportsChatbotCrew
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -13,13 +13,16 @@ load_dotenv()
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-if __name__ == "__main__":
-    crew = SportsChatbotCrew()
-    # Example user input and structured input for player info
-    user_input = "Tell me about Virat Kohli's recent performance."
-    structured_input = {
-        "player_name": "Virat Kohli",
-        "sport": "cricket",
-        "time_period": "last 5 matches"
+def run():
+    """ Run the crew """
+    
+    inputs ={
+        "query":"Give me information about Virat Kohli, who plays cricket"
     }
-    print(crew.handle_user_request(user_input, structured_input))
+    
+    SportsChatbotCrew().crew().kickoff(inputs=inputs)
+    
+
+    
+    
+    
